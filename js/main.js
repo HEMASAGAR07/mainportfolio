f6346cca259f6d521119669c0e85d00e90b1218a
SANISETTY HEMA SAGAR <93524527+HEMASAGAR07@users.noreply.github.com>
2023-02-25 00:32:24 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ swup.on('contentReplaced', update);
 var temp = 0;
 
 function update() {
-    if (window.location.href == 'https://hemasagar07.github.io/mainportfolio/index.html') {
+    if (window.location.href == 'https://hemasagar07.github.io/mainportfolio/') {
         document.getElementById('mm0').classList.add('active');
         document.getElementById('mm1').classList.remove('active');
         document.getElementById('mm2').classList.remove('active');
@@ -54,24 +54,24 @@ function update() {
 
     }
     if (temp == 0) {
-        document.getElementById('m1').innerHTML = '<a href="/aboutMe.html"><i class="fas fa-chevron-right"></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/publications.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="aboutMe.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="publications.html"><i class="fas fa-chevron-left"></i></i></i></a>';
     }
     if (temp == 1) {
-        document.getElementById('m1').innerHTML = '<a href="/acadamics.html"><i class="fas fa-chevron-right"></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/index.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="acadamics.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="index.html"><i class="fas fa-chevron-left"></i></i></i></a>';
     }
     if (temp == 2) {
-        document.getElementById('m1').innerHTML = '<a href="/skills.html"><i class="fas fa-chevron-right"></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/aboutMe.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="skills.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="aboutMe.html"><i class="fas fa-chevron-left"></i></i></i></a>';
     }
     if (temp == 3) {
-        document.getElementById('m1').innerHTML = '<a href="/publications.html"><i class="fas fa-chevron-right"></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/acadamics.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="publications.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="acadamics.html"><i class="fas fa-chevron-left"></i></i></i></a>';
     }
     if (temp == 4) {
-        document.getElementById('m1').innerHTML = '<a href="/index.html"><i class="fas fa-chevron-right"></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/skills.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="index.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="skills.html"><i class="fas fa-chevron-left"></i></i></i></a>';
     }
 
 }
